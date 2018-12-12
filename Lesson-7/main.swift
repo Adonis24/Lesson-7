fef12c93266221793d199584804eabfc42794f44
r.chernetsov <r.chernetsov@adonis24.ru>
2018-12-12 22:04:52 +0300

    Initial Commit

---
+++ Lesson-7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson-7
+//
+//  Created by Чернецова Юлия on 12/12/2018.
+//  Copyright © 2018 Чернецов Роман. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
